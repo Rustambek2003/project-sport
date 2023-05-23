d7492edf32cc1d3534d2891d75f085aa2ee7aec3
Rustambek <125789903+Rustambek2003@users.noreply.github.com>
2023-05-23 12:55:18 +0600

    asdf

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,6 +11,7 @@ import Sport from '../pages/Sports/Sport'
 import { Route,Routes } from 'react-router-dom'
 import Contact from '../pages/Contact/Contact'
 
+
 const App = () => {
   return (
     <>
@@ -26,6 +27,8 @@ const App = () => {
           <Route path="/pikure" element={<Pikure />} />
           <Route path="/sport" element={<Sport />} />
           <Route path="/contact" element={<Contact />} />
+          
+
     
         </Routes>
     
